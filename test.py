3bbe905412cd453874ca22812c8ffbe01e6efd79
Ismail EL JAMIY <104994203+Ismailjm@users.noreply.github.com>
2024-06-14 15:22:00 +0100

    Committing test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+from langdetect import detect, detect_langs
+
+text = "how are you"
+
+# Detect the language
+language = detect(text)
+print(f"The detected language is: {language}")
+
+# Detect language probabilities
+probabilities = detect_langs(text)
+print(f"Language probabilities: {probabilities}")
