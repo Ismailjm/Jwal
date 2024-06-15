d643c8c7a79460964f0885caebebbe597c93d0a0
Ismail EL JAMIY <104994203+Ismailjm@users.noreply.github.com>
2024-06-15 15:20:10 +0100

    Committing questions.py

---
+++ utils/questions.py
@@ -0,0 +1,10 @@
+questions = {
+   "age": "How old are you?",
+  "gender": "What is your gender? (Please answer with 'male', 'female', or 'other')",
+  "disability": "Do you have any disabilities? (Please answer with 'yes' or 'no')",
+  "activities_type": "What kind of activities do you want to do? (Kid Friendly, Museums, Shopping, Historical, Outdoor Adventures, Art & Cultural, Amusement Parks)",
+  "destinations": "Do you have any desired destinations in Morocco?",
+  "duration_of_travel": "How long do you plan to travel?",
+  "budget_level": "What is your budget level? (Cheap, Mid, High) or specify your budget"
+}
+
